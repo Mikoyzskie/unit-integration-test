8035fef5b67e781d59cca3570ac6e8f125fdafa7
Mikoyzskie <rapidstrike13@gmail.com>
2023-10-08 02:30:37 +0800

    module export update

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {}
 
-module.exports = nextConfig
+module.exports = {
+    nextConfig,
+    rules: {
+		'testing-library/no-await-sync-events': [
+			'error',
+			{ eventModules: ['fire-event', 'user-event'] },
+		],
+	},
+}
